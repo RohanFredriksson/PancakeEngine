5880b302399f7de40eca5a9daa372e49c9548239
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2022-12-03 14:11:46 +1100

    Improved build scripts.

--- build.bat
+++ build.bat
@@ -1,3 +1,14 @@
+@echo off
+if not exist "build" mkdir build
 cd build
+if exist posix (
+    del CMakeCache.txt /s /f /q > nul
+) else (
+    if not exist windows (
+        del CMakeCache.txt /s /f /q > nul
+    )
+)
+del posix /s /f /q > nul
+type nul > windows
 cmake .. -G "MinGW Makefiles"
 mingw32-make
--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+mkdir build 2> /dev/null
 cd build
+if [ -f windows ]; then
+    rm CMakeCache.txt
+elif [ ! -f posix ]; then
+    rm CMakeCache.txt
+fi
+rm windows 2> /dev/null
+touch posix
 cmake ..
 make
