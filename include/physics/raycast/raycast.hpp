59e18bc7b956473852575b5a726b1dbebe3bc94c
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2022-12-07 22:01:11 +1100

    Added raycast methods.

---
+++ include/physics/raycast/raycast.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include "physics/primitives/rigidbody.hpp"
+
+using glm::vec2;
+
+class Ray {
+  
+    public:
+
+        vec2 origin;
+        vec2 direction;
+
+        Ray(vec2 origin, vec2 direction);
+
+};
+
+class RaycastResult {
+
+    public:
+
+        vec2 point;
+        vec2 normal;
+        float distance;
+        Entity* hit;
+
+        RaycastResult(vec2 point, vec2 normal, float distance, Entity* hit);
+        RaycastResult();
+        
+};
+
+namespace Raycast {
+    RaycastResult Raycast(Rigidbody* rigidbody, Ray* ray);
+}
