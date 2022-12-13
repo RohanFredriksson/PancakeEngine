906a99d6fa99dc28256be80e413deadff7d201a6
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2022-12-13 16:59:04 +1100

    Added soloud to the project with little test script.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
-#include "core/window.hpp"
+#include "soloud/include/soloud.h"
+#include "soloud/include/soloud_speech.h"
+#include "soloud/include/soloud_thread.h"
 
-using namespace Window;
+//#include "core/window.hpp"
+//using namespace Window;
 
 int main() {
-    run();
+    
+    // Define a couple of variables
+    SoLoud::Soloud soloud;  // SoLoud engine core
+    SoLoud::Speech speech;  // A sound source (speech, in this case)
+
+    // Configure sound source
+    speech.setText("1 2 3   1 2 3   Hello world. Welcome to So-Loud.");
+
+    // initialize SoLoud.
+    soloud.init();
+
+    // Play the sound source (we could do this several times if we wanted)
+    soloud.play(speech);
+
+    // Wait until sounds have finished
+    while (soloud.getActiveVoiceCount() > 0)
+    {
+        // Still going, sleep for a bit
+        SoLoud::Thread::sleep(100);
+    }
+
+    // Clean up SoLoud
+    soloud.deinit();
+
+    // All done.
+    return 0;
+    
+    //run();
 }
