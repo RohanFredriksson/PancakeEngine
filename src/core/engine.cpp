ab48ef39b698d8fc3a89a5462fe713dd09cdadf8
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2023-03-25 15:15:23 +1100

    Loaded the factory on initialisation.

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -1,10 +1,28 @@
 #include "pancake/core/engine.hpp"
+#include "pancake/core/factory.hpp"
 #include "pancake/audio/audioengine.hpp"
 #include "pancake/window/window.hpp"
+
+#include "pancake/audio/audioplayer.hpp"
+#include "pancake/graphics/renderer/spriterenderer.hpp"
+#include "pancake/graphics/renderer/textrenderer.hpp"
+#include "pancake/physics/primitives/rigidbody.hpp"
+
+namespace {
+
+    void registry() {
+        Factory::add("AudioPlayer", AudioPlayer::create);
+        Factory::add("SpriteRenderer", SpriteRenderer::create);
+        Factory::add("TextRenderer", TextRenderer::create);
+        Factory::add("Rigidbody", Rigidbody::create);
+    }
+
+}
 
 namespace Engine {
 
     int run(string name, string filename, void(*init)(Scene* scene)) {
+        registry();
         AudioEngine::init();
         if (!Window::init(name, filename, init)) {return 1;}
         Window::loop();
@@ -14,6 +32,7 @@ namespace Engine {
     }
 
     int run(string name, void (*init)(Scene* scene)) {
+        registry();
         AudioEngine::init();
         if (!Window::init(name, init)) {return 1;}
         Window::loop();
@@ -23,6 +42,7 @@ namespace Engine {
     }
 
     int run(string name, string filename) {
+        registry();
         AudioEngine::init();
         if (!Window::init(name, filename)) {return 1;}
         Window::loop();
